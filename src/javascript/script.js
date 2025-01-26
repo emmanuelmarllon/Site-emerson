f1c336709a1e6ede34156f5137f881e093090a26
emmanuelmarllon <marlonebr339@gmail.com>
2025-01-26 18:45:13 -0300

    .

---
+++ src/javascript/script.js
@@ -0,0 +1,17 @@
+let menuList = document.getElementById("menuList");
+let content = document.querySelector("main");
+
+menuList.style.maxHeight = "0"; // Inicializa o menu fechado
+
+function toggleMenu() {
+  // Verifica o estado atual do menu
+  if (menuList.style.maxHeight === "0px") {
+    // Abre o menu
+    menuList.style.maxHeight = "300px"; // Altura do menu quando aberto
+    content.classList.add("open-menu"); // Empurra o conteúdo para baixo
+  } else {
+    // Fecha o menu
+    menuList.style.maxHeight = "0px"; // Fecha o menu
+    content.classList.remove("open-menu"); // Restaura o conteúdo para o topo
+  }
+}
